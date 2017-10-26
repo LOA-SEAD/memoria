be6536f519490febeb5a6b108adf410b04cc7b9c
Delano Medeiros Beder <delano@dc.ufscar.br>
2017-10-26 16:19:44 -0200

    Remove "Descrição:" text

--- js/script.sobreTexto.js
+++ js/script.sobreTexto.js
@@ -19,7 +19,7 @@ $.ajax({
 sobre.padrao = new Array();
 
 sobre.padrao[0] = "<h3 style='text-align: left; position: relative; left:-50px; top: 40px;  font: 40px comic sans ms'>";
-sobre.padrao[1] = "</h3><br><b>Descrição:</b> ";
+sobre.padrao[1] = "</h3><br>";
 
 sobre.infoCartas = new Array();
 
